30e673982d5881b34ee70ca3df59b232fca0f502
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-18 01:05:00 +0530

    Basic utilities

---
+++ src/utils/apiError.js
@@ -0,0 +1,23 @@
+class apiError extends Error{
+    constructor(
+        statuscode,
+        message,
+        errors=[],
+        stack="",
+
+    ){
+        super(message);
+        this.statuscode=statuscode;
+        this.data=null;
+        this.message=message,
+        this.success=false,
+        this.errors=errors
+        if(stack){
+            this.stack=stack
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+export default apiError;
