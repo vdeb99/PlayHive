17ac76cca3ded97693fb644eb5626c2d869056a8
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-23 22:07:14 +0530

    Added subscribers/channel features using mongodb pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -146,4 +146,127 @@ const refreshToken=asyncHandler(async(req,res)=>{
     }
 
 })
-export {registerUser,loginUser,logoutUser,refreshToken};
+const changeCurrentPassword=asyncHandler(async(req,res)=>{
+    const {oldPassword,newPassword}=req.body
+    const user=await User.findById(req.user?._id)
+    const isPasswordCorrect=await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new apiError(400,"Invalid password")
+    }
+    user.password=newPassword
+    await user.save({validateBeforeSave:false});
+    return res
+    .status(200)
+    .json(new apiResponse(200,{},"Password changed successfully"))
+
+})
+const getCurrentUser=asyncHandler(async(req,res)=>{
+    return res
+    .status(200)
+    .json(200,req.user,"User fetched successfully")
+})
+const updateAccountDetails=asyncHandler(async(req,res)=>{
+    const {fullName,email}=req.body
+    if(!fullName && !email){
+        throw new apiError(400,"Full name or email is required")
+    }
+    const user=User.findByIdAndUpdate(req.user._id,{$set:{fullName,email}},{new:true}).select("-password -refreshToken")
+    return res
+    .status(200)
+    .json(new apiResponse(200,user,"User updated successfully"))
+})
+
+const updateUserAvatar=asyncHandler(async(req,res)=>{
+    const avatarLocalPath=req.file?.path
+    if(!avatarLocalPath){
+        throw new apiError(400,"Avatar is required")
+    }
+    const avatar=await uploadToCloudinary(avatarLocalPath)
+    if(!avatar.url){
+        throw new apiError(400,"Error uploading avatar")
+    }
+    const user=await User.findByIdAndUpdate(req.user?._id,{$set:{avatar:avatar.url}}).select("-password -refreshToken")
+
+})
+const updateUserCoverImage=asyncHandler(async(req,res)=>{
+    const coverImageLocalPath=req.file?.path
+    if(!coverImageLocalPath){
+        throw new apiError(400,"Cover image is required")
+    }
+    const coverImage=await uploadToCloudinary(coverImageLocalPath)
+    if(!coverImage.url){
+        throw new apiError(400,"Error uploading avatar")
+    }
+    const user=await User.findByIdAndUpdate(req.user?._id,{$set:{coverImage:coverImage.url}}).select("-password -refreshToken")
+    return res
+    .status(200)
+    .json(new apiResponse(200,user,"Cover image updated successfully"))
+})
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+    const {username}=req.params
+    if(!username?.trim()){
+        throw new apiError(400,"Username is required")
+    }
+    const channel=await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                subscribesToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{
+                            $in:[req.user?._id,"$subscribers.subscriber"]
+                        },
+                        then:true,
+                        else:false
+                    }
+                    
+                }
+            }
+        },{
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                subscribesToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+            }
+        }
+    ])
+    if(!channel?.length){
+        throw new apiError(404,"Channel not found")
+    }
+    return res
+    .status(200)
+    .json(new apiResponse(200,channel[0],"Channel fetched successfully"))
+})
+export {registerUser,loginUser,logoutUser,refreshToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage};
