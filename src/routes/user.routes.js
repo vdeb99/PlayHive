deb292c98c8598485dc83a51975dc1955083e91c
Prantick Deb <prantickdeb2005@gmail.com>
2025-02-26 12:24:03 +0530

    Added user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import {verifyJwt} from "../middlewares/auth.middleware.js"
 import {upload} from "../middlewares/multer.middleware.js"
-import  {registerUser,loginUser,logoutUser,refreshToken}  from "../controllers/user.controller.js";
+import  {registerUser,loginUser,logoutUser,refreshToken, changeCurrentPassword, getCurrentUser,
+     updateAccountDetails, updateUserAvatar, updateUserCoverImage,
+      getUserChannelProfile, getWatchHistory}  
+      from "../controllers/user.controller.js";
 const router=Router();
 router.route("/register").post(
     upload.fields([
@@ -18,4 +21,11 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJwt,logoutUser)
 router.route("/refresh-token").post(refreshToken)
+router.route("/change-password").post(verifyJwt,changeCurrentPassword)
+router.route("/current-user").get(verifyJwt,getCurrentUser)
+router.route("/update-account").patch(verifyJwt,updateAccountDetails)
+router.route("/update-avatar").patch(verifyJwt,upload.single("avatar"),updateUserAvatar)
+router.route("/update-cover-image").patch(verifyJwt,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJwt,getUserChannelProfile)
+router.route("/watch-history").get(verifyJwt,getWatchHistory)
 export default router;
