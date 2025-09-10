e284ece0089223fd23174d865452c95a5bab0825
Prantick Deb <prantickdeb2005@gmail.com>
2025-09-10 11:09:14 +0530

    Final Commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 import  connectDB  from "./db/db.js";
 dotenv.config({path:"../.env"})
 connectDB()
-.then(()=>{
+
+.then((req,res)=>{
+    res.send("Hello from server")
+    res.json({message:"Hello from server"})
     app.listen(process.env.PORT || 8000 ,()=>{
         console.log(`Server is running on port ${process.env.PORT || 8000}`)
     })
